12c68287900f4405d89006dd29caad98cb827382
Lautaro Torres Mariotti <lautaromt.94@gmail.com>
2022-10-26 22:59:40 -0300

    Se sube challenge

---
+++ src/index.js
@@ -0,0 +1,52 @@
+import fetch from "node-fetch"
+import fs from "fs"
+import { stringify } from "csv-stringify";
+
+
+const URL_API = "https://api.mercadolibre.com" //search?q=chromecast&limit=50#json"
+
+const PRODUCTS_TYPES = ['chromecast', 'apple tv', 'amazon fire tv','google home']
+
+const FILENAME = "ComparativeOfProducts-Meli.csv"
+const writableStream = fs.createWriteStream(FILENAME)
+
+const columns = [
+    "title",
+    "price",
+    "base_price",
+    "initial_quantity",
+    "condition"
+]
+
+const stringifier = stringify({header:true, columns:columns, delimiter:',', bom: true})
+
+async function dataFetch({url}) {
+    const res = await fetch(url)
+    const data = res.json()
+    return data
+}
+
+async function generateCSV({products}) {
+    products.forEach(product => {
+        const url = `${URL_API}/items/${product.id}`
+        dataFetch({url}).then(res => {
+            const data = {
+                title : res.title.replace(',',''),
+                price : res.price,
+                base_price : res.base_price,
+                initial_quantity : res.initial_quantity,
+                condition : res.condition
+            }
+            stringifier.write(data)
+        })
+    })
+}
+
+PRODUCTS_TYPES.forEach(type => {
+    const url = `${URL_API}/sites/MLA/search?q=${type}&limit=50`
+    dataFetch({url})
+        .then(data => generateCSV({products:data.results}))
+})
+
+stringifier.pipe(writableStream);
+
